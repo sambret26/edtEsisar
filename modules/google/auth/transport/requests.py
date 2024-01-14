a71536856a39761d2624ceeeae62d6cd3038b544
sambret26 <77980006+sambret26@users.noreply.github.com>
2024-01-14 15:06:36 +0100

    Update requests.py

--- modules/google/auth/transport/requests.py
+++ modules/google/auth/transport/requests.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
 
+sys.path.append("../../../../")
 """Transport adapter for Requests."""
 
 from __future__ import absolute_import
