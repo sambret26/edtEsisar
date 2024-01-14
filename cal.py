46d8882319dfb6fc765c41d15ce7a5b693e1150b
Samuel Bretiere <samuel.bretiere@etu.esisar.grenoble-inp.fr>
2024-01-14 17:01:23 +0100

    Updated cal.py

--- cal.py
+++ cal.py
@@ -151,8 +151,9 @@ def findCreds(area):
   creds = None
   timetable = DB.getTimetable(area)
   fileName = "token" + str(timetable)
-  if path.exists("DB/" + fileName + ".pkl"):
-    creds = load(fileName)
+  #if path.exists("DB/" + fileName + ".pkl"):
+    #creds = load(fileName)
+  creds = os.environ.get('TOKEN3A')
   if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
